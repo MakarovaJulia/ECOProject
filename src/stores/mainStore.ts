8ec0bf28a313b87929ca274bb9e9b88b558f8c87
MakarovaJulia <julmakan@gmail.com>
2022-03-09 19:35:52 +0300

    Added stores

---
+++ src/stores/mainStore.ts
@@ -0,0 +1,13 @@
+import { ModalStore } from './modalStore'
+
+export class MainStore{
+    modalStore: ModalStore;
+
+    constructor() {
+        this.modalStore = new ModalStore(this)
+    }
+}
+
+const mainStore = new MainStore();
+
+export default mainStore
