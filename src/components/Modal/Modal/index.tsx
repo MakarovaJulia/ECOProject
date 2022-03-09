34f3629eeb67a98939a274044102e63964677e3a
MakarovaJulia <julmakan@gmail.com>
2022-03-09 17:10:12 +0300

    Added new file

---
+++ src/components/Modal/Modal/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Modal = (props: any) => {
+    const {
+        children
+    } = props;
+
+    return (
+        <>
+            Hello
+        </>
+    )
+}
